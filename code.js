1e237900f6921873a9de66a844cbd6f63e336d58
unknown <web.35.faruk@gmail.com>
2022-01-06 09:52:15 +0600

    first commit

---
+++ code.js
@@ -0,0 +1,7 @@
+
+const loginButton   =  document.getElementById('login');
+loginButton.addEventListener('click',function () {
+   const login_page    = document.getElementById('login_page');  
+   login_page.style.display = "none";
+})     
+ 
